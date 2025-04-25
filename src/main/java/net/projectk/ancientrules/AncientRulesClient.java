80f1a3830c7b61a3ee90015a53c5f13552831398
Fungus Defender <capybara.okipullup121@gmail.com>
2025-04-25 15:56:52 +0700

    Beta

---
+++ src/main/java/net/projectk/ancientrules/AncientRulesClient.java
@@ -0,0 +1,10 @@
+package net.projectk.ancientrules;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientRulesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/projectk/ancientrules/AncientRulesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.projectk.ancientrules;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AncientRulesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
